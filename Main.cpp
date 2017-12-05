67efc436c0ca9bec1499012cdb7dd2324cc9e586
inaenomaki <kimama.0410@gmail.com>
2017-12-05 19:03:41 +0900

    Main.cppを空にした

--- Main.cpp
+++ Main.cpp
@@ -1,21 +1,5 @@
-#include"longer_int.h"
-#include<iostream>
+
 
 int main(void) {
-	const int ary_num = 7;
-	longer_int hoge[ary_num];
-
-	hoge[0] = 1;//int‚Ì‘ã“ü
-
-	hoge[1] = "2";//std::string‚Ì‘ã“ü
-
-	hoge[2] = hoge[0] + hoge[1];//longer_int + longer_int
-
-	hoge[3] = hoge[0] + 3;//longer_int + int
-
-	hoge[4] = 3 + hoge[0];//int + longer_int
-
-	for (int i = 0; i < ary_num; i++) {
-		std::cout << hoge[i].str() << std::endl;
-	}
+	
 }
