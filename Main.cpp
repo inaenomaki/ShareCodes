3966ea0b8e5074131bde6081c57a1324a0ce26fd
inaenomaki <makiofinae@gmail.com>
2018-07-12 22:02:23 +0900

    ポインタが指すオブジェクトの寿命の簡単な確認

--- Main.cpp
+++ Main.cpp
@@ -1,21 +1,29 @@
 #include<iostream>
 #include"myFunc.h"
 
+void func(int **p) {
+	int huga = 10;//discarded at end of function
+	*p = &huga;
+}
+
 int main(void) {
-	int ary[5];
-	int hoge = 5;
-	int* hoge_p = &hoge;
+	int	*p;//address
+	int hoge = 2;//value
+	p = &hoge;
 
+	cout_instant(p);
+	cout_instant(*p);
+	
+	{//scope test
+		int bar = 4;
+		p = &bar;
+	}
+	
+	cout_instant(p);//not discarded?
+	cout_instant(*p);//not discarded?
 
-	//以下簡易表示用マクロ
-	cout_instant(ary);//array's name is read as pointer to the head of the array. But,ary is pointer to array that has 5 elements.
-	cout_instant(&ary[0]);//&ary[0] is pointer to ary's initial element.But,ary is pointer to int.
-
-	cout_instant(sizeof(ary));//ary is pointer to array that has 5 elements.
-	cout_instant(sizeof(&ary[0]));//ary is pointer to int.
-
-	cout_instant(sizeof(hoge));//hoge is int.
-	cout_instant(sizeof(hoge_p));// hoge_p is pointer to int.
-
+	func(&p);
+	cout_instant(p);//discarded
+	cout_instant(*p);//discarded
 
 }
